620b2dbff6da5473b89e9820844f0aaf05dc9833
N8N <nathanello@users.noreply.github.com>
2023-03-30 15:07:23 -0400

    Final SEO Updates

--- src/components/SEO/NextHeadBase.tsx
+++ src/components/SEO/NextHeadBase.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
 
 const NextHeadBase = () => {
-  const baseUrl = "https://downloads.notjustjpgs.com/";
+  const baseUrl = "https://download-video-games.netifly.app/";
   const ogImgUrl = `${baseUrl}/images/splash.png`;
-  const title = "LoopGate — Token-Gated Content using Loopring NFTs";
+  const title = "NJJ Publishing — Token-Gated Videoo Game Downloads";
   const description =
-    "Securely and easily access exclusive digital content with LoopGate, using Loopring NFTs.";
+    "Securely and easily access exclusive digital downloads. Powered by LoopGate.";
 
   return (
     <Head>
@@ -15,7 +15,7 @@ const NextHeadBase = () => {
       <meta property="og:type" content="website" />
       <meta property="og:url" content={baseUrl} />
       <meta property="og:image" content={ogImgUrl} />
-      <meta property="og:site_name" content="LoopGate" />
+      <meta property="og:site_name" content="NJJ Publishing — Token-Gated Videoo Game Downloads" />
       <meta property="og:locale" content="en_US" />
 
       <meta name="twitter:card" content="summary_large_image" />
