415426155f575cf3509c3a23da68ade58f067582
N8N <nathanello@users.noreply.github.com>
2023-03-30 14:06:14 -0400

    Update Config

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,23 +21,10 @@ Here are two examples:
 
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
+    cid: "bafybeibnx7wm5ijomzugkfp7ghk7cbqikhfjgjfta3joz4v5pa6in57aem", // BB AUS TOUR
     nftId: [
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' to unlock this.
-  },
-  {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
-  },
-  {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
+      "0x29e59d237e1d8c72b8f36fa85a4d86d02d3b413945cd4cbff41cfa77e5b34d83",
+    ], // You need to own BB AUS TOUR to unlock this.
   },
 ];
 
