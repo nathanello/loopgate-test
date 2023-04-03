d0e39adca17ab07cacf009fa4633eaec7aeb96a4
N8N <nathanello@users.noreply.github.com>
2023-04-03 14:45:27 -0400

    Update Config

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,10 +21,16 @@ Here are two examples:
 
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeibnx7wm5ijomzugkfp7ghk7cbqikhfjgjfta3joz4v5pa6in57aem", // BB AUS TOUR
+    cid: "bafybeibbfcrjpu3ib5mipxpyo2joz7eed4bvft6def5ckcmy4scfaxvnqa", // BB AUS TOUR
     nftId: [
       "0x29e59d237e1d8c72b8f36fa85a4d86d02d3b413945cd4cbff41cfa77e5b34d83",
-    ], // You need to own BB AUS TOUR to unlock this game download.
+    ], // You need to own BB AUS TOUR to unlock this.
+  },
+  {
+    cid: "bafybeiagzjl775o2vpigiucoe2of6n2qigfjfthdra62lj74vsexk6coxq", // BB TAP
+    nftId: [
+      "0x418267ed4f086a7a8fa7152434340ba09ec77081ac358b8b6599942c3e3773bd",
+    ], // You need to own BB TAP to unlock this.
   },
 ];
 
